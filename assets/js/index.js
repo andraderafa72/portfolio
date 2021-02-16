8c72542841474e79b0d23a94924449e976e9f087
Rafael Andrade <devrafael72@gmail.com>
2021-02-16 01:56:47 -0300

    Hero and portfolio area completed

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('.front-end-projects').slick({
+    dots: true,
+    infinite: true,
+    arrows: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  });
+  $('.node-projects').slick({
+    dots: true,
+    infinite: true,
+    arrows: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  });
+});
